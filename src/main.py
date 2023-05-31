005a8b662eaa02f367474b70d0685d974748020d
Мария Лапикова <mashik_p@mail.ru>
2023-06-01 01:00:31 +0300

    fixed imports

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 from configs import configure_argument_parser, configure_logging
-from constants import BASE_DIR, MAIN_DOC_URL, MAIN_PEP_URL, EXPECTED_STATUS
+from constants import BASE_DIR, EXPECTED_STATUS, MAIN_DOC_URL, MAIN_PEP_URL
 from outputs import control_output
 from utils import find_tag, get_response
 
